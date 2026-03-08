6a57a4467b6ee738c0f1161908f6af3ebc49b897
admin123261 <flyminingcloud@gmail.com>
2026-03-08 20:48:08 +0100

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { loadFromSupabase } from "./supabaseSync";
+import App from "./App";
 
+// تحميل البيانات من Supabase أولاً ثم تشغيل التطبيق
+loadFromSupabase().finally(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
